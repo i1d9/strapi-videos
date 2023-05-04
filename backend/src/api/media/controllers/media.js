eb6b37179e433c5508738a48079e567f5f41dc98
Ian Naylan <42600121+i1d9@users.noreply.github.com>
2023-05-04 14:44:04 +0300

    media controller update: check if file exists

--- backend/src/api/media/controllers/media.js
+++ backend/src/api/media/controllers/media.js
@@ -3,26 +3,40 @@
 /**
  * media controller
  */
-const fs = require('fs');
+const fs = require('fs');// To interact with the file system
+const path = require('path');
+
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
 module.exports = createCoreController('api::media.media', ({ strapi }) => ({
+
     async play(ctx) {
         let { video } = await strapi.entityService.findOne('api::media.media', ctx.request.params.id, {
-
             populate: {
                 video: true
             },
         });
         ctx.set('Content-Type', 'video/mp4')
         ctx.set('Accept-Ranges', 'bytes')
+
         //Get path from db
-        const videoPath = "YOUR_PATH" + video.url;
+        const MY_PATH = strapi.dirs.static.public;
+        const videoPath = path.join(MY_PATH, video.url);
+
+        // Check if the file exists
+        try {
+            await fs.promises.access(videoPath, fs.constants.F_OK);
+            console.log('The file exists');
+        } catch (err) {
+            ctx.throw(404, `File not found: ${videoPath}`);
+        }
+
         const videoStat = fs.statSync(videoPath);
         const fileSize = videoStat.size;
         const videoRange = ctx.request.headers.range;
-        //Some browsers send a range in the initial request, but others don’t. 
+
+        // Some browsers send a range in the initial request, but others don’t. 
         if (videoRange) {
             const parts = videoRange.replace(/bytes=/, "").split("-");
             const start = parseInt(parts[0], 10);
@@ -34,11 +48,11 @@ module.exports = createCoreController('api::media.media', ({ strapi }) => ({
             ctx.set('Content-Length', chunksize);
             ctx.set('Content-Range', `bytes ${start}-${end}/${fileSize}`);
             ctx.body = fs.createReadStream(videoPath, { start, end })
-
         } else {
             ctx.status = 200
             ctx.set('Content-Length', fileSize)
             ctx.body = fs.createReadStream(videoPath)
         }
     },
+
 }));
